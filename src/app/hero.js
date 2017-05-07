15ad40aa56809449a24d17b91235c2de009a19ea
ZongJie <2660038273@qq.com>
2017-05-07 23:01:57 +0800

    first commit

---
+++ src/app/hero.js
@@ -0,0 +1,14 @@
+"use strict";
+var Hero = (function () {
+    function Hero(id, name, power, alterEgo) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.alterEgo = alterEgo;
+    }
+    return Hero;
+}()); /**
+ * Created by ZongJie on 2017/5/7.
+ */
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ src/app/hero.ts
@@ -0,0 +1,9 @@
+export class Hero {
+  constructor(public id: number,
+              public name: string,
+              public power: string,
+              public alterEgo?: string) {
+  }
+}/**
+ * Created by ZongJie on 2017/5/7.
+ */
